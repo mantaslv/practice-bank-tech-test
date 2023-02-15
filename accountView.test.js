40ae10ef1fa7469e144b4ce1f577eb81e3ff41ef
Mantas Volkauskas <mantasvolkas@gmail.com>
2023-02-15 21:47:26 +0000

    Tested: prints statement of multiple varied transactions

--- accountView.test.js
+++ accountView.test.js
@@ -29,10 +29,34 @@ describe(AccountView, () => {
             [
                 { date: '14/01/2023', credit: null, debit: 500, balance: 2500 }
             ]
-        );  // 14/01/2023 || || 500.00 || 2500.00
+        );
 
         const accountView = new AccountView(mockAccountModel);
         expect(accountView.printStatement())
             .toBe('date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00');
+    });
+
+    it('prints formatted statement of multiple varied transactions', () => {
+        const mockAccountModel = new AccountModel();
+
+        mockAccountModel.getTransactions.mockImplementation(() => 
+            [
+                { date: '10/01/2023', credit: 1000, debit: null, balance: 1000 },
+                { date: '13/01/2023', credit: 2000, debit: null, balance: 3000 },
+                { date: '14/01/2023', credit: null, debit: 500, balance: 2500 }
+            ]
+        );
+
+        const accountView = new AccountView(mockAccountModel);
+        expectedStatementOutput = [
+            'date || credit || debit || balance',
+            '14/01/2023 || || 500.00 || 2500.00',
+            '13/01/2023 || 2000.00 || || 3000.00',
+            '10/01/2023 || 1000.00 || || 1000.00'
+        ]
+        .join('\n');
+
+        expect(accountView.printStatement())
+            .toBe(expectedStatementOutput)
     });
 });
