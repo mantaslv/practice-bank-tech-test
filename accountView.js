85b7ad68f05c0111a0baf0038844655015760502
Mantas Volkauskas <mantasvolkas@gmail.com>
2023-02-15 21:33:50 +0000

    Refactored by adding 2dp formatter method

--- accountView.js
+++ accountView.js
@@ -2,32 +2,31 @@ class AccountView {
     constructor(accountModel) {
         this.accountTransactions = accountModel.getTransactions();
     }
-
-    // .toBe('date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00');
-
+    
     printStatement() {
         const header = 'date || credit || debit || balance';
-
-        console.log(this.accountTransactions);
-        
         const body = this.accountTransactions
             .map((el) => (
                 [
                     el.date,
-                    el.credit === null ? "" : el.credit.toFixed(2),
-                    el.debit === null ? "" : el.debit.toFixed(2), 
-                    el.balance.toFixed(2)
+                    this.formatToTwoDP(el.credit),
+                    this.formatToTwoDP(el.debit),
+                    this.formatToTwoDP(el.balance)
                 ]
                 .join(' || ')
                 .replace(/  +/g, ' ')
             ));
 
-        console.log(body);
-        console.log(header);
-
         body.push(header);
-
         return body.reverse().join('\n');
+    }
+
+    formatToTwoDP(number) {
+        if (number === null) {
+            return "";
+        } else {
+            return number.toFixed(2);
+        };
     }
 }
 
